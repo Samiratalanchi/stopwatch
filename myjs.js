04d42b1403724c5e6a7af7e12db997146bd205ec
samira talanchi <samira73talanchi@gmail.com>
2021-12-05 19:20:31 +0330

    fix start function bugs

--- myjs.js
+++ myjs.js
@@ -6,19 +6,19 @@ function start() {
 }
 function startWatch() {
     sec++;
-    if(sec<10) {
+    if(sec<10 || sec==0) {
         sec = "0" + parseInt(sec);
     } else if(sec==60) {
         min++;
-        sec=0;
+        sec="00";
     }
-    if(min<10) {
+    if(min<10 || min==0) {
         min = "0" + parseInt(min);
     } else if(min==60) {
         hour++;
-        min=0;
+        min="00";
     }
-    if(hour<10){
+    if(hour<10 || hour==0){
        hour = "0" + parseInt(hour); 
     }
     document.getElementById("timer").innerHTML = hour + ":" + min + ":" + sec; 

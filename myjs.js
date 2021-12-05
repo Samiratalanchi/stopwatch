98ed437403a278cf023b76a90015e50626708ce5
samira talanchi <samira73talanchi@gmail.com>
2021-12-05 19:14:43 +0330

    add start function

---
+++ myjs.js
@@ -0,0 +1,25 @@
+var sec = 0;
+var min = 0;
+var hour = 0;
+function start() {
+    setInterval(startWatch,1000);
+}
+function startWatch() {
+    sec++;
+    if(sec<10) {
+        sec = "0" + parseInt(sec);
+    } else if(sec==60) {
+        min++;
+        sec=0;
+    }
+    if(min<10) {
+        min = "0" + parseInt(min);
+    } else if(min==60) {
+        hour++;
+        min=0;
+    }
+    if(hour<10){
+       hour = "0" + parseInt(hour); 
+    }
+    document.getElementById("timer").innerHTML = hour + ":" + min + ":" + sec; 
+}
